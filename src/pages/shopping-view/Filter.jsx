507c67e4edc82442253a34b3a209564b25fd2f67
sahal2255 <sahalvv9656@gmail.com>
2025-06-23 22:04:54 +0530

    filter component setup

--- src/pages/shopping-view/Filter.jsx
+++ src/pages/shopping-view/Filter.jsx
@@ -1,5 +1,8 @@
-import React from "react";
-
+import React, { Fragment } from "react";
+import { filterOptions } from "@/config/AllConfig";
+import { Label } from "@/components/ui/label";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Separator } from "@/components/ui/separator";
 const ProductFilter = () => {
   return (
     <div className="bg-background rounded-lg shadow-sm">
@@ -7,11 +10,26 @@ const ProductFilter = () => {
         <h2 className="text-lg font-semibold">Filters</h2>
       </div>
       <div className="p-4 space-y-4">
-        {/* {Object.keys(filterOptions).map(keyItems=>{
-
-        }
-    )
-        } */}
+        {Object.keys(filterOptions).map((keyItem) => (
+          <Fragment key={keyItem}>
+            <div>
+              <h3 className="text-base font-bold capitalize">{keyItem}</h3>
+              <div className="grid gap-2 mt-2">
+                {filterOptions[keyItem].map((option) => (
+                  <Label
+                    key={option.id}
+                    htmlFor={`${keyItem}-${option.id}`}
+                    className="flex items-center gap-2 font-normal"
+                  >
+                    <Checkbox id={`${keyItem}-${option.id}`} />
+                    {option.label}
+                  </Label>
+                ))}
+              </div>
+            </div>
+            <Separator />
+          </Fragment>
+        ))}
       </div>
     </div>
   );
--- src/pages/shopping-view/Products.jsx
+++ src/pages/shopping-view/Products.jsx
@@ -5,6 +5,16 @@ const Products = () => {
   return (
     <div className='grid grid-cols-1 md:grid-cols-[300px_1fr] gap-6 p-4 '>
       <ProductFilter />
+      <div className=' bg-background w-full rounded-lg shadow-sm'>
+        <div className=' p-4 border-b flex items-center justify-between'>
+          <h2 className='text-lg font-extrabold'>All Products</h2>
+          <div className=' felx items-center gap-2'>
+            <span className='text-muted-foreground'>10 Products</span>
+            
+          </div>
+        </div>
+
+      </div>
     </div>
   )
 }
